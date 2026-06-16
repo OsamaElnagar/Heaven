57184f26b763d43d5819b89bf44933bea1326835
Osama Elnagar <osamaelngar98@gmail.com>
2026-06-16 12:00:49 +0300

    Updating proposal to be for Heaven instead of Wosoul

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    'email' => 'info@wosoul-travel.com',
+    'email' => 'info@heaven-travel.com',
 
     /*
     |--------------------------------------------------------------------------
